4d0c85577b53d1ceb6ac6c7c42ddf6d9023c71e6
chrisaiv <mail@chrisaiv.com>
2012-11-13 14:15:34 -0800

    A few more updates

--- tiztheseason.jit.su/public/js/app.js
+++ tiztheseason.jit.su/public/js/app.js
@@ -1,33 +1,11 @@
-$("a#link").bind("click", onClickHandler );
-function onClickHandler(e){
-	console.log( "onClickHandler:", $(this).attr("href") );
-	filepicker.exportFile(
-		$(this).attr("href"),
-		{mimetype:'image/png'},
-		function(FPFile){
-			console.log(FPFile.url);
-		}
-	);
-	return false
-}
+function includeFooter( date ){
 
-function onChangeHandler(e ){
-	if( e.filename != "undefined") onCompleteHandler(e)
-	else onErrorHandler(e);
-}
-
-function onCompleteHandler( fpfile ){
-    $(".confirm").html("<h2>Success</h2>");
-    $(".confirm").html('<p>You just uploaded '+
-    fpfile.filename+'! '+
-    'You can download the file at '+ 
-    "<a href='#' id='link'><img src='" + fpfile.url + "' border='0' /></a></p>" );
-
-    $(".confirm").show();
-}
-function onErrorHandler( fperror ){
-    console.log("onErrorHandler:", fperror );
-}
-
-function sendMail(){
+	var now = date;
+	var testPatterns = [
+		'yyyy','yy','MMMM','MMM','MM','M','dd','d','EEEE','EEE','a',
+		'HH','H','hh','h','mm','m','ss','s','S',
+		'EEEE MMMM, d yyyy hh:mm:ss.S a',
+		'M/d/yy HH:mm'
+	];
+	$("footer span").append( $.formatDate( now, 'yyyy' ) );	
 }
